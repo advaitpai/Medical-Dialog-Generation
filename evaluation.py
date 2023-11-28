487fb0fee744726a1867a8de9dd9a4d1698bc737
zohairhashmi <hashmi.zohair@gmail.com>
2023-11-28 00:19:58 -0600

    Evaluation code updated

--- evaluation.py
+++ evaluation.py
@@ -2,17 +2,21 @@ import pickle
 import pandas as pd
 from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 from bert_score import BERTScorer
-import tqdm
 import time
+import evaluate
 
 def tokenize(text):
     return text.split()
 
-def bleu_score(test_df):
+def bleu_score(test_df, evaluation_type):
 
-    # tokenize doctor dialogs and responses
+    if evaluation_type == 'context':
+        reference = test_df['context']
+    elif evaluation_type == 'doctor_dialog':
+        reference = test_df['doctor_dialog']
+
     response_tokens = [tokenize(r) for r in test_df['response']]
-    docdialogs_tokens = [tokenize(d) for d in test_df['doctor_dialog']]
+    reference_tokens = [tokenize(r) for r in reference]
 
     scores = []
     count = 0
@@ -23,7 +27,7 @@ def bleu_score(test_df):
             # if test_df['context'] is not empty calculate bleu score else skip the sample
             if test_df['context'][i] != '[]':
                 # Calculate BLEU score
-                score = corpus_bleu([docdialogs_tokens[i]], [response_tokens[i]], smoothing_function=smoothing_function)
+                score = corpus_bleu([reference_tokens[i]], [response_tokens[i]], smoothing_function=smoothing_function)
                 scores.append(score)
                 count += 1
             else:
@@ -35,7 +39,13 @@ def bleu_score(test_df):
 
     return scores
 
-def bert_score(test_df):
+def bert_score(test_df, evaluation_type):
+
+    if evaluation_type == 'context':
+        reference = test_df['context']
+    elif evaluation_type == 'doctor_dialog':
+        reference = test_df['doctor_dialog']
+
     scorer = BERTScorer(model_type='bert-base-uncased')
     reference_dialogs = []
     candidate_responses = []
@@ -43,7 +53,7 @@ def bert_score(test_df):
         try:
             # if test_df['context'] is not empty add doctor dialog and response to list else skip the sample
             if test_df['context'][i] != '[]':
-                reference_dialogs.append(test_df['doctor_dialog'][i])
+                reference_dialogs.append(reference[i])
                 candidate_responses.append(test_df['response'][i])
             else:
                 continue
@@ -54,37 +64,45 @@ def bert_score(test_df):
     print(f"BERT score calculated for {len(F1)} out of {len(test_df)} samples")
     return P, R, F1
 
-def perplexity_score(context, response):
-    return
+def rouge_score(test_df, evaluation_type):
+    if evaluation_type == 'context':
+        reference = test_df['context']
+    elif evaluation_type == 'doctor_dialog':
+        reference = test_df['doctor_dialog']
 
+    reference_dialogs = []
+    candidate_responses = []
+    for i in range(len(test_df)):
+        try:
+            # if test_df['context'] is not empty add doctor dialog and response to list else skip the sample
+            if test_df['context'][i] != '[]':
+                reference_dialogs.append(reference[i])
+                candidate_responses.append(test_df['response'][i])
+            else:
+                continue
+        except KeyError as e:
+            continue
+    
+    rouge = evaluate.load('rouge')
+    results = rouge.compute(predictions=test_df['response'], references=reference)
+    print(f"ROUGE score calculated for {len(results)} out of {len(test_df)} samples")
 
-if __name__ == "__main__":
+    return results
 
-    test_samples = pd.read_pickle("datasets/test_samples.pkl").reset_index(drop=True)
-    # extract 0 to100 and and 200 to 300 samples
-    test_samples_1 = test_samples.iloc[0:100]
-    test_samples_2 = test_samples.iloc[200:300]
-    test_samples = pd.concat([test_samples_1, test_samples_2], ignore_index=True)
-    doctor_dialogs = test_samples['doctor_dialog']
-    generated_responses = pd.read_csv("datasets/responses_all.csv")
-    responses = generated_responses['response']
-    context = generated_responses['context']
-
-    # create dataframe having columns doctor dialogs and responses and context
-    df = pd.DataFrame({'doctor_dialog': doctor_dialogs, 'response': responses, 'context': context})
-
-    print('length of actual doctor dialogs: ', len(doctor_dialogs))
-    print('length of responses: ', len(responses))
-    print('\n')
-
-
+def run_evaluation(evaluation_type, df):
+    print(f"Evaluation type: {evaluation_type}")
     # Calculate BLEU score for test set
     start_time = time.time()
-    b_scores = bleu_score(df)         
-    # save to text file
-    with open('datasets/bleu_scores.txt', 'w') as f:
-        for item in b_scores:
-            f.write("%s\n" % item)
+    b_scores = bleu_score(df, evaluation_type=evaluation_type)         
+    # Save to text file
+    if evaluation_type == 'context':
+        with open('datasets/bleu_scores_context.txt', 'w') as f:
+            for item in b_scores:
+                f.write("%s\n" % item)
+    elif evaluation_type == 'doctor_dialog':
+        with open('datasets/bleu_scores_ground_truth.txt', 'w') as f:
+            for item in b_scores:
+                f.write("%s\n" % item)
 
     avg_b_score = sum(b_scores)/len(b_scores)
     end_time = time.time()
@@ -92,22 +110,56 @@ if __name__ == "__main__":
 
     # Calculate BERT score
     start_time = time.time()
-    P, R, F1 = bert_score(df)
-    # save F1 scores text file
-    with open('datasets/bert_scores.txt', 'w') as f:
-        for item in F1:
-            f.write("%s\n" % item)
-    end_time = time.time()
+    P, R, F1 = bert_score(df, evaluation_type=evaluation_type)
+    # Save F1 scores text file
+    if evaluation_type == 'context':
+        with open('datasets/bert_scores_context.txt', 'w') as f:
+            for item in F1:
+                f.write("%s\n" % item)
+    elif evaluation_type == 'doctor_dialog':
+        with open('datasets/bert_scores_ground_truth.txt', 'w') as f:
+            for item in F1:
+                f.write("%s\n" % item)
     print(f"Time taken to calculate BERT score: {end_time-start_time}")
-    
+
+    # Calculate ROUGE score
+    start_time = time.time()
+    r_score = rouge_score(df, evaluation_type=evaluation_type)
+    # Save ROUGE scores text file
+    if evaluation_type == 'context':
+        with open('datasets/rouge_scores_context.txt', 'w') as f:
+            for item in r_score:
+                f.write("%s\n" % item)
+    elif evaluation_type == 'doctor_dialog':
+        with open('datasets/rouge_scores_ground_truth.txt', 'w') as f:
+            for item in r_score:
+                f.write("%s\n" % item)
+    end_time = time.time()
+    print(f"Time taken to calculate ROUGE score: {end_time-start_time}")
+
     print('\n')
     print('--'*20)
-    print(f"Average BLEU Score: {avg_b_score}")
+    print(f"Average BLEU Score: {avg_b_score:.4f}")
     print(f"BERT Score (Mean F1): {F1.mean():.4f}")
+    print(f"ROUGE score: {r_score['rougeL']:0.4f}")
+
+    print('Evaluation completed for: ', evaluation_type)
+    print('--'*20)
+    print('--'*20)
+    print('\n')
 
 
-    # Calculate perplexity score
 
-    # # Calculate ROUGE score for test set
-    # r_score = rouge_score(context, responses)
-    # print(f"ROUGE Score: {r_score}")
+if __name__ == "__main__":
+    test_samples = pd.read_pickle("datasets/test_samples.pkl").reset_index(drop=True)
+    generated_responses = pd.read_csv("datasets/responses_all.csv")
+
+    doctor_dialogs = test_samples['doctor_dialog']
+    responses = generated_responses['response']
+    context = generated_responses['context']
+
+    df = pd.DataFrame({'doctor_dialog': doctor_dialogs, 'response': responses, 'context': context})
+
+    # run evaluation for ground truth and context
+    run_evaluation(evaluation_type='doctor_dialog', df=df)
+    run_evaluation(evaluation_type='context', df=df)
